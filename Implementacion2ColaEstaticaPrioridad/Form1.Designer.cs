2094ef8809b73458648e35b7fbee1027ce17c409
CuentaDeGitHub <60906225+CuentaDeGitHub@users.noreply.github.com>
2020-11-05 01:18:46 -0600

    Add files via upload

---
+++ Implementacion2ColaEstaticaPrioridad/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Implementacion2ColaEstaticaPrioridad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCola0 = new System.Windows.Forms.Label();
+            this.lblCola1 = new System.Windows.Forms.Label();
+            this.lblCola2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPrioridad = new System.Windows.Forms.TextBox();
+            this.txtDato = new System.Windows.Forms.TextBox();
+            this.btnIncrementar = new System.Windows.Forms.Button();
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.btnDesencolar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTamaño = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prioridad 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prioridad 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Prioridad 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dato";
+            // 
+            // lblCola0
+            // 
+            this.lblCola0.AutoSize = true;
+            this.lblCola0.Location = new System.Drawing.Point(91, 25);
+            this.lblCola0.Name = "lblCola0";
+            this.lblCola0.Size = new System.Drawing.Size(37, 13);
+            this.lblCola0.TabIndex = 4;
+            this.lblCola0.Text = "Cola 0";
+            // 
+            // lblCola1
+            // 
+            this.lblCola1.AutoSize = true;
+            this.lblCola1.Location = new System.Drawing.Point(91, 50);
+            this.lblCola1.Name = "lblCola1";
+            this.lblCola1.Size = new System.Drawing.Size(37, 13);
+            this.lblCola1.TabIndex = 5;
+            this.lblCola1.Text = "Cola 1";
+            // 
+            // lblCola2
+            // 
+            this.lblCola2.AutoSize = true;
+            this.lblCola2.Location = new System.Drawing.Point(91, 74);
+            this.lblCola2.Name = "lblCola2";
+            this.lblCola2.Size = new System.Drawing.Size(37, 13);
+            this.lblCola2.TabIndex = 6;
+            this.lblCola2.Text = "Cola 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Prioridad";
+            // 
+            // txtPrioridad
+            // 
+            this.txtPrioridad.Location = new System.Drawing.Point(71, 141);
+            this.txtPrioridad.Name = "txtPrioridad";
+            this.txtPrioridad.Size = new System.Drawing.Size(32, 20);
+            this.txtPrioridad.TabIndex = 8;
+            // 
+            // txtDato
+            // 
+            this.txtDato.Location = new System.Drawing.Point(71, 113);
+            this.txtDato.Name = "txtDato";
+            this.txtDato.Size = new System.Drawing.Size(32, 20);
+            this.txtDato.TabIndex = 9;
+            // 
+            // btnIncrementar
+            // 
+            this.btnIncrementar.Location = new System.Drawing.Point(221, 167);
+            this.btnIncrementar.Name = "btnIncrementar";
+            this.btnIncrementar.Size = new System.Drawing.Size(75, 23);
+            this.btnIncrementar.TabIndex = 13;
+            this.btnIncrementar.Text = "Incrementar";
+            this.btnIncrementar.UseVisualStyleBackColor = true;
+            this.btnIncrementar.Click += new System.EventHandler(this.btnIncrementar_Click);
+            // 
+            // btnEncolar
+            // 
+            this.btnEncolar.Location = new System.Drawing.Point(9, 167);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(94, 23);
+            this.btnEncolar.TabIndex = 14;
+            this.btnEncolar.Text = "Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click);
+            // 
+            // btnDesencolar
+            // 
+            this.btnDesencolar.Location = new System.Drawing.Point(140, 167);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesencolar.TabIndex = 15;
+            this.btnDesencolar.Text = "Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Click += new System.EventHandler(this.btnDesencolar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtArchivo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.btnDesencolar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnEncolar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnIncrementar);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtDato);
+            this.groupBox1.Controls.Add(this.lblCola0);
+            this.groupBox1.Controls.Add(this.txtPrioridad);
+            this.groupBox1.Controls.Add(this.lblCola1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblCola2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 245);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(87, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Nombre del archivo";
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(90, 205);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(100, 20);
+            this.txtArchivo.TabIndex = 17;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(9, 205);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 16;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Tamaño de las colas";
+            // 
+            // txtTamaño
+            // 
+            this.txtTamaño.Location = new System.Drawing.Point(117, 13);
+            this.txtTamaño.Name = "txtTamaño";
+            this.txtTamaño.Size = new System.Drawing.Size(31, 20);
+            this.txtTamaño.TabIndex = 20;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(154, 11);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 21;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(240, 11);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 22;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.btnCargar);
+            this.groupBox2.Controls.Add(this.btnAceptar);
+            this.groupBox2.Controls.Add(this.txtTamaño);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(321, 45);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 308);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCola0;
+        private System.Windows.Forms.Label lblCola1;
+        private System.Windows.Forms.Label lblCola2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPrioridad;
+        private System.Windows.Forms.TextBox txtDato;
+        private System.Windows.Forms.Button btnIncrementar;
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.Button btnDesencolar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTamaño;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
